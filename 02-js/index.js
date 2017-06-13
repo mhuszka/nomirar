462184e92527b2fd129446f0886f87b15a908b9d
mhuszka <melissa.bojama@gmail.com>
2017-06-13 15:30:18 +0200

    nouveau hover

--- 02-js/index.js
+++ 02-js/index.js
@@ -3,3 +3,23 @@ $('.txt').html(function(i, html) {
 
   return '<span>' + chars.join('</span><span>') + '</span>';
 });
+
+var imagehover1 = document.querySelector('.image-hover1');
+var project1 = document.querySelector('.image-preview1');
+
+imagehover1.classList.add('hidden');
+project1.addEventListener('mouseover', appear);
+
+function appear(){
+    imagehover1.classList.remove('hidden');
+}
+
+var imagehover2 = document.querySelector('.image-hover2');
+var project2 = document.querySelector('.image-preview2');
+
+imagehover2.classList.add('hidden');
+project2.addEventListener('mouseover', appear2);
+
+function appear2(){
+    imagehover2.classList.remove('hidden');
+}
