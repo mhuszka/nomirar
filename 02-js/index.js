f6a9da304efd11bf00cc74aa6a1d1f93cc4b7cdb
mhuszka <melissa.bojama@gmail.com>
2017-06-13 15:36:26 +0200

    modif du js

--- 02-js/index.js
+++ 02-js/index.js
@@ -1,8 +1,8 @@
-$('.txt').html(function(i, html) {
-  var chars = $.trim(html).split("");
-
-  return '<span>' + chars.join('</span><span>') + '</span>';
-});
+//$('.txt').html(function(i, html) {
+//  var chars = $.trim(html).split("");
+//
+//  return '<span>' + chars.join('</span><span>') + '</span>';
+//});
 
 var imagehover1 = document.querySelector('.image-hover1');
 var project1 = document.querySelector('.image-preview1');
